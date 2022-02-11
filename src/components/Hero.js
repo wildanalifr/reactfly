b8c36303e878912b41db6d3704ff11d9ce81b2ba
BB\Wildan <wildanalifrio@gmail.com>
2022-02-11 20:44:20 +0700

    first

---
+++ src/components/Hero.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Button } from 'reactstrap'
+import Heros from '../images/heros.png'
+
+const Hero = () => {
+  return (
+    <div className="container mt-5 d-flex align-items-center">
+      <div className="">
+        <h1 className="display-3">Hello, Semua!</h1>
+        <p className="lead">Selamat Datang di Personal Website ku, Wildan Dev 😊</p>
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <p className="lead">
+          <Button color="primary">Click Me </Button>
+        </p>
+      </div>
+
+      <div>
+        <img src={Heros} />
+      </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  Nav,
+  Collapse,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from 'reactstrap'
+
+const navbar = () => {
+  return (
+    <div>
+      <Navbar color="light" expand="md" light>
+        <NavbarBrand href="/">WilDev</NavbarBrand>
+        <NavbarToggler />
+        <Collapse navbar>
+          <Nav className="me-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">My Social Media</NavLink>
+            </NavItem>
+            <UncontrolledDropdown inNavbar nav>
+              <DropdownToggle caret nav>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  )
+}
+
+export default navbar
